f7d72db22b61660cfc501a6dc186fcb4a6bdc7ac
Khansa <108682140+khansa-ta@users.noreply.github.com>
2022-08-14 17:26:29 +0700

    Update Saucedemo-test.py

--- Saucedemo-test.py
+++ Saucedemo-test.py
@@ -1,13 +1,7 @@
-from cgitb import text
-from lib2to3.pgen2 import driver
 import unittest
 import time
-from urllib import response
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 class Test_a_Login(unittest.TestCase):
@@ -24,9 +18,10 @@ class Test_a_Login(unittest.TestCase):
         driver.find_element(By.ID,"password").send_keys(12345678)
         time.sleep(1)
         driver.find_element(By.ID,"login-button").click()
+        time.sleep(3)
 
-        response_message = driver.find_element(By.CSS_SELECTOR,"#login_button_container > div > form > div.error-message-container.error > h3 > button").text
-        self.assertIn(response_message,"Username and password do not match")
+        response_data = driver.find_element(By.CSS_SELECTOR,"#login_button_container > div > form > div.error-message-container.error > h3 > button").text
+        self.assertIn(response_data, "Username and password do not match")
      
     def test_b_Login_Successful(self):
         driver = self.driver
@@ -37,8 +32,9 @@ class Test_a_Login(unittest.TestCase):
         driver.find_element(By.ID,"password").send_keys("secret_sauce")
         time.sleep(1)
         driver.find_element(By.ID,"login-button").click()
-
-        response_data= driver.find_element(By.ID,"page_wrapper")
+        time.sleep(3)
+        
+        self.assertEqual(driver.current_url, "https://www.saucedemo.com/inventory.html")
     
     def test_c_Login_EmptyUser(self):
         driver = self.driver
@@ -49,9 +45,10 @@ class Test_a_Login(unittest.TestCase):
         driver.find_element(By.ID,"password").send_keys("secret_sauce")
         time.sleep(1)
         driver.find_element(By.ID,"login-button").click()
+        time.sleep(3)
 
-        response_message = driver.find_element(By.CSS_SELECTOR,"#login_button_container > div > form > div.error-message-container.error > h3 > button").text
-        self.assertIn(response_message,"Username is required")
+        response_data = driver.find_element(By.CSS_SELECTOR,"#login_button_container > div > form > div.error-message-container.error > h3 > button").text
+        self.assertIn(response_data,"Username is required")
     
     def test_d_Login_EmptyPass(self):
         driver = self.driver
@@ -62,9 +59,10 @@ class Test_a_Login(unittest.TestCase):
         driver.find_element(By.ID,"password").send_keys("")
         time.sleep(1)
         driver.find_element(By.ID,"login-button").click()
+        time.sleep(3)
 
-        response_message = driver.find_element(By.CSS_SELECTOR,"#login_button_container > div > form > div.error-message-container.error > h3 > button").text
-        self.assertIn(response_message,"Password is required")
+        response_data = driver.find_element(By.CSS_SELECTOR,"#login_button_container > div > form > div.error-message-container.error > h3 > button").text
+        self.assertIn(response_data, "Password is required")
 
 class Test_b_AddtoCart(unittest.TestCase):
 
@@ -80,14 +78,17 @@ class Test_b_AddtoCart(unittest.TestCase):
         driver.find_element(By.ID,"password").send_keys("secret_sauce")
         time.sleep(1)
         driver.find_element(By.ID,"login-button").click()
+        time.sleep(1)
         driver.find_element(By.CSS_SELECTOR,"#add-to-cart-sauce-labs-backpack").click()
-        
-        response_message = driver.find_element(By.CLASS_NAME,"shopping_cart_badge").text
-        self.assertEqual(response_message, "1")
+        time.sleep(1)
+
+        response_data = driver.find_element(By.CLASS_NAME,"shopping_cart_badge").text
+        self.assertEqual(response_data, "1")
 
     def tearDown(self): 
         self.driver.close() 
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
 
     
